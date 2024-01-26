3ca290535f784d7daf1d7d908aab31eb00128c4f
AlexCeja-157 <el.ktrin157@gmail.com>
2024-01-25 21:21:03 -0600

    Fin de la primera sesion

---
+++ backend/routes/tareasRoutes.js
@@ -0,0 +1,18 @@
+const express = require ('express')
+const router = express.Router()
+const { getTareas, createTareas, updateTareas, deleteTareas} = require ('../Controllers/tareasController')
+
+
+//router.route('/').get(getTareas).post(createTareas)
+router.get('/', getTareas)
+router.post('/', createTareas)
+
+
+//router.route('/:id').put(updateTareas).delete(deleateTareas)
+router.put('/:id', updateTareas)
+router.delete('/:id', deleteTareas)
+
+
+
+module.exports = router
+
