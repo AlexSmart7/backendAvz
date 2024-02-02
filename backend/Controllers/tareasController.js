05a64fa3709f173e4b8c82c41075b48684f52494
AlexCeja-157 <el.ktrin157@gmail.com>
2024-02-01 20:38:36 -0600

    terminado

--- backend/Controllers/tareasController.js
+++ backend/Controllers/tareasController.js
@@ -1,18 +1,26 @@
-const getTareas = (req,res) => {
+const asyncHandler = require('express-async-handler')
+
+const getTareas = asynHandler( async (req,res) => {
     res.status(200).json({mensaje: 'Get Tareas' })
-}
+})
 
-const createTareas = (req,res) => {
-    res.status(200).json({mensaje: 'Create Tareas' })
-}
+const createTareas = asynHandler (async(req,res) => {
+    
+    if(!req.body.descripcion) {
+        res.status(400)
+        throw new Error('Por favor teclea una descripcion')
+    }
 
-const updateTareas = (req,res) => {
+    res.status(201).json({mensaje: 'Create Tareas' })
+})
+
+const updateTareas = asyncHandler( async(req,res) => {
     res.status(200).json({mensaje: ` Modificar la tarea con id ${req.params.id} ` })
-}
+})
 
-const deleteTareas = (req,res) => {
+const deleteTareas = asyncHandler( async(req,res) => {
     res.status(200).json({ id: req.params.id  })
-}
+})
 
 
 module.exports = {
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
+const asyncHandler =require('express-async-handler')
+
+
 const express = require('express')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddelware')
 const port = process.env.PORT || 5000
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended:false}))
 
 app.use('/api/tareas', require('./routes/tareasRoutes'))
 
