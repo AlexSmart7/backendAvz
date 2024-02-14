10027e93009a363a4f99e9d9f0bbb4320b7dcb76
AlexCeja-157 <el.ktrin157@gmail.com>
2024-02-14 02:14:17 -0600

    Agregando CORS

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,15 @@ const colors = require('colors')
 const connectDB = require('./config/db')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddelware')
+const cors = require('cors')
 const { connect } = require('mongoose')
 const port = process.env.PORT || 5000
 
 connectDB()
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended:false}))
